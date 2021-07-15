d83abc0f60ac58f676c877bb87b74dd4f2c626e0
özge salman <49956992+sozgesalman@users.noreply.github.com>
2021-07-15 14:18:11 +0300

    feat(component): Add UIComponent and GamePlayComponent script
    to be developed

---
+++ Assets/Scripts/UIComponent.cs
@@ -0,0 +1,16 @@
+namespace IWPAGamesProject.Component
+{
+    using Devkit.Base.Component;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class UIComponent : MonoBehaviour
+    {
+        public void Initialize(ComponentContainer componentContainer)
+        {
+            
+        }        
+    }
+}
+
