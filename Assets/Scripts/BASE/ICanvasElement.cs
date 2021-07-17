8025cc93cf6ad77313b240068050d2ce7ae1493c
özge salman <49956992+sozgesalman@users.noreply.github.com>
2021-07-17 17:31:17 +0300

    feat(base): BaseCanvas completed

---
+++ Assets/Scripts/BASE/ICanvasElement.cs
@@ -0,0 +1,16 @@
+namespace IWPAGamesProject.UserInterface
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public interface ICanvasElement
+    {
+        void Init();
+
+        void Activate();
+
+        void Deactivate();
+    }
+}
+
