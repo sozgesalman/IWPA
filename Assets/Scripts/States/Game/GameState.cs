376bb342e0a05c181eef95fca3770e7f22d7147b
özge salman <49956992+sozgesalman@users.noreply.github.com>
2021-07-17 17:23:02 +0300

    feat(state): Add states to use

---
+++ Assets/Scripts/States/Game/GameState.cs
@@ -0,0 +1,29 @@
+namespace IWPAGamesProject.State
+{   
+    using Devkit.Base.Component;
+    using Devkit.SM;
+    using IWPAGamesProject.Component;
+    using IWPAGamesProject.UserInterface;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class GameState : StateMachine
+    {
+        protected override void OnEnter()
+        {
+
+        }
+
+        protected override void OnUpdate()
+        {
+
+        }
+
+        protected override void OnExit()
+        {
+            
+        }
+        
+    }
+}
